ae07e4fbc458c6336c6f7a6b57a459b11b01af9a
Damaris C <DCampos07@users.noreply.github.com>
2020-12-09 19:16:43 -0500

    thougths model added
    added schema for Thoughts and reaction schema

---
+++ models/Thought.js
@@ -0,0 +1,73 @@
+const { Schema, model, Types } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+const ReactionSchema = new Schema(
+  {
+    // set custom id to avoid confusion with parent comment _id
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId()
+    },
+    reactionBody: {
+      type: String,
+      required: "You must enter a reaction",
+      trim: true,
+      minlength: [1, "Oh no! That's not enough characters."],
+      maxlength: [280, "Opps, that's too many characters"]
+    },
+    
+    userName: {
+      type: String,
+      required: "Seems you forgot to write your username"
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: createdAtVal => dateFormat(createdAtVal)
+    }
+  },
+  {
+    toJSON: {
+      getters: true
+    }
+  }
+);
+
+const ThoughtSchema = new Schema(
+  {
+    writtenBy: {
+      type: String,
+      required: "Seems you forgot to write your username"
+    },
+    thoughtText: {
+      type: String,
+      required: "You must enter a reaction",
+      trim: true,
+      minlength: [1, "Oh no! That's not enough characters."],
+      maxlength: [280, "Opps, that's too many characters"]
+
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: createdAtVal => dateFormat(createdAtVal)
+    },
+    // use ReactionSchema to validate data for a reply
+    replies: [ReactionSchema]
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true
+    },
+    id: false
+  }
+);
+
+ThoughtSchema.virtual('reactionCount').get(function() {
+  return this.reactoins.length;
+});
+
+const Thought = model('Thought', ThoughtSchema);
+
+module.exports = Thought;
